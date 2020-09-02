98c2cea5fe9b2d3b19d0a1122785cad7da1d9a1d
gabidias <silva.gdias@gmail.com>
2020-09-02 16:22:12 -0300

    Klingon ok

--- salve.py
+++ salve.py
@@ -1,11 +1,17 @@
 # --start
 import sys
 
+def klingon():  #add
+    print('nuqneH')  #add
+
 def default():
     print('o que voce quer?')
 
 def main():
-    default()
+    if sys.argv[1] == 'klingon':  #add
+       klingon()  #add
+    else:
+       default()
 
 if __name__ == '__main__':
     main()
