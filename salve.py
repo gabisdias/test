bac4c85a9068fcb8ac702ca2130ee87b677d777a
gabidias <silva.gdias@gmail.com>
2020-09-02 16:36:42 -0300

    Volcanook

--- salve.py
+++ salve.py
@@ -1,11 +1,17 @@
 # --start
 import sys
 
+def volcano():  #add
+    print('dif-tor heh smusma')  #add
+
 def default():
     print('o que voce quer?')
 
 def main():
-    default()
+    if sys.argv[1] == 'volcano':  #add
+       volcano()  #add
+    else:
+       default()
 
 if __name__ == '__main__':
     main()
